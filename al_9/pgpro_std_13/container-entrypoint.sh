9bb167a2001c8ba9ed1e8298e477a1c159a79743
kostik-pl <kostik_pl@hotmail.com>
2024-09-12 21:22:10 +0300

    UPDATE

--- al_9/pgpro_std_13/container-entrypoint.sh
+++ al_9/pgpro_std_13/container-entrypoint.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/_data/srv1c_inf_log" ] ; then
-	mkdir /_data/pg_backup
-fi
-if [ ! -d "/_data/srv1c_inf_log" ] ; then
-	mkdir /_data/pg_data
-fi
-chown -R postgres:postgres /_data/pg_backup
-chmod -R 733 /_data/pg_backup
-chown -R postgres:postgres /_data/pg_data
-chmod -R 700 /_data/pg_data
-
 set -e
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
