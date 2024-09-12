0647dc7427bf06b9c8953e51df8bc7031b70de15
kostik-pl <kostik_pl@hotmail.com>
2024-09-12 11:41:06 +0300

    UPDATE

--- al_8/main/install2.1_podman_pgpro_13_std.sh
+++ al_8/main/install2.1_podman_pgpro_13_std.sh
@@ -26,7 +26,7 @@ chmod -R 700 /_data/pg_data
 #Change the image name to the desired image. Example kostikpl/ol9:pgpro_1c_13 > kostikpl/rhel8:pgpro_std_13
 echo 'Pull and setup container...'
 HOSTNAME=`hostname`
-podman run --shm-size 2G --name pgpro --hostname $HOSTNAME -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/al_9:pgpro_std_13
+podman run --name pgpro --shm-size 2G -d -p 5432:5432 -v /_data:/_data docker.io/kostikpl/al_8:pgpro_std_13
 podman generate systemd --new --name pgpro > /etc/systemd/system/pgpro.service
 systemctl enable --now pgpro
 firewall-cmd --permanent --add-service=postgresql
@@ -36,8 +36,3 @@ PG_PASSWD='RheujvDhfub72'
 podman exec -ti pgpro psql -c "ALTER USER postgres WITH PASSWORD '$PG_PASSWD';"
 #srv1c_PASSWD = '\$GitybwZ - ZxvtyM\$' # $GitybwZ - ZxvtyM$
 #podman exec -ti pgpro psql -c "ALTER USER srv1c WITH PASSWORD '$srv1c_PASSWD';"
-podman run --name pgadmin -d -p 5050:80 -e 'PGADMIN_LISTEN_ADDRESS=0.0.0.0' -e 'PGADMIN_DEFAULT_EMAIL=k.druchevsky@kernel.ua' -e 'PGADMIN_DEFAULT_PASSWORD=a1502EMC2805' docker.io/dpage/pgadmin4
-podman generate systemd --new --name pgpro > /etc/systemd/system/pgadmin.service
-systemctl enable --now pgadmin
-firewall-cmd --permanent --add-port=5050/tcp
-firewall-cmd --reload
--- al_9/main/install2.1_podman_pgpro_13_std.sh
+++ al_9/main/install2.1_podman_pgpro_13_std.sh
@@ -36,8 +36,3 @@ PG_PASSWD='RheujvDhfub72'
 podman exec -ti pgpro psql -c "ALTER USER postgres WITH PASSWORD '$PG_PASSWD';"
 #srv1c_PASSWD = '\$GitybwZ - ZxvtyM\$' # $GitybwZ - ZxvtyM$
 #podman exec -ti pgpro psql -c "ALTER USER srv1c WITH PASSWORD '$srv1c_PASSWD';"
-podman run --name pgadmin -d -p 5050:80 -e 'PGADMIN_LISTEN_ADDRESS=0.0.0.0' -e 'PGADMIN_DEFAULT_EMAIL=k.druchevsky@kernel.ua' -e 'PGADMIN_DEFAULT_PASSWORD=a1502EMC2805' docker.io/dpage/pgadmin4
-podman generate systemd --new --name pgpro > /etc/systemd/system/pgadmin.service
-systemctl enable --now pgadmin
-firewall-cmd --permanent --add-port=5050/tcp
-firewall-cmd --reload
